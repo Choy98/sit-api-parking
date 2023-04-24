aad03c223fb79d1fcf402ec16a3ad613aa08de48
Choy <choy98@outlook.com.pe>
2023-04-23 19:17:53 -0500

    feat: Implementar controladores para regisdtrar entrada y salida de a…

--- api-parking/src/main/java/com/parking/apiparking/controller/ParkingController.java
+++ api-parking/src/main/java/com/parking/apiparking/controller/ParkingController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/parking") //parking
@@ -15,22 +16,55 @@ import java.util.List;
 public class ParkingController {
     // INYECCION DE DEPENDENCIAS
 
-  private final ParkingService parkingService;
     @Autowired
-    public ParkingController(ParkingService parkingService){
-       this.parkingService = parkingService;
-   }
+    private ParkingService parkingService;
     /// GET : http://dominio/parking/cars  Obtener la lista de carros
    @GetMapping("/cars")
    public List getAllCars(){
         return this.parkingService.getAllCars();
    }
 
+    @GetMapping("/cars/{licensePlate}")
+    public ResponseEntity<Car> getCarByLicensePlate(@PathVariable String licensePlate){
+        Optional<Car> optionalCar = this.parkingService.getCarByLicensePlate(licensePlate);
+        if(optionalCar.isPresent()){
+            return ResponseEntity.ok(optionalCar.get());
+        }
+        return ResponseEntity.notFound().build();
+    }
+    @GetMapping("/cars")
+    public ResponseEntity<List<Car>> getCarsByColor(@RequestParam(required = false)String color){
+        if(color != null){
+            List<Car> carsByColor = this.parkingService.getCarsByColor(color);
+            return ResponseEntity.ok(carsByColor);
+        }
+        return ResponseEntity.ok(this.parkingService.getAllCars());
+    }
    //POST: http://dominio/parking/cars
     @PostMapping("/cars")
     public ResponseEntity<Car> addCar(@RequestBody Car car){
         this.parkingService.addCar(car);
         return new ResponseEntity<>(car, HttpStatus.CREATED);
+    }
+    @DeleteMapping("/cars/{licensePlate}")
+    public ResponseEntity<Void> removeCarByLicensePlate(@PathVariable String licensePlate){
+        boolean removed = this.parkingService.removeCarByLicensePlate(licensePlate);
+        if (removed){
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+    @PostMapping("/cars/{licensePlate}/entry")
+    public ResponseEntity<Void> registerEntry(@PathVariable String licensePlate){
+        Car car = new Car(licensePlate, null);
+        this.parkingService.parkCar(car);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+    @PostMapping("/cars/{licensePlate}/exit")
+    public ResponseEntity<Double> registerExit(@PathVariable String licensePlate){
+        double fee = this.parkingService.calculateParkingFeee(licensePlate);
+        this.parkingService.unpackCar(licensePlate);
+        return ResponseEntity.ok(fee);
     }
 
 
